59c3bc837e8dcb00e58503f7ff0cc68dda7b7d3d
Kuecumber <kuecumber0@gmail.com>
2026-07-30 21:24:14 -0400

    Main JSS Fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,18 +19,20 @@
     headerToggleBtn.classList.toggle('bi-list');
     headerToggleBtn.classList.toggle('bi-x');
   }
-  headerToggleBtn.addEventListener('click', headerToggle);
+
+  if (headerToggleBtn) {
+    headerToggleBtn.addEventListener('click', headerToggle);
+  }
 
   /**
    * Hide mobile nav on same-page/hash links
    */
   document.querySelectorAll('#navmenu a').forEach(navmenu => {
     navmenu.addEventListener('click', () => {
-      if (document.querySelector('.header-show')) {
+      if (document.querySelector('.header-show') && headerToggleBtn) {
         headerToggle();
       }
     });
-
   });
 
   /**
